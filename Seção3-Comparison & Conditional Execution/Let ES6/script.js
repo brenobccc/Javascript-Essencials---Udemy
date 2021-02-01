bcbfa35e8a4a7a1d60761e3d256204157d9d0b6b
brenobccc <brenobritobc@gmail.com>
2021-02-01 10:38:44 -0300

    midificação_final

---
+++ Seção3-Comparison & Conditional Execution/Let ES6/script.js
@@ -0,0 +1,10 @@
+
+var symbolName = "value reference in memory";
+//referência de alor na memória
+
+let letSimbol = "scoped value";
+//valor do escopo
+
+const symName = "constant value in memory";
+//valor constante na memória
+
